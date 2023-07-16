a7bc48111d4b0dc6b7d287ce9abf274ec07aca7d
DineoMaela <139717146+DineoMaela@users.noreply.github.com>
2023-07-17 00:08:33 +0200

    Add files via upload

---
+++ GreetingdFrameApp.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package greetingdframeapp;
+
+import za.ac.tut.gui.GreetindGUI;
+
+/**
+ *
+ * @author game
+ */
+public class GreetingdFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GreetindGUI greet;
+        greet = new GreetindGUI();
+    }
+    
+}
